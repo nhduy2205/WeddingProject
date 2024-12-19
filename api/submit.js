bcebf79a979f9e9aa601cfac5536cf0a95d9292b
nhduy <nhduy@ctu.edu.vn>
2024-12-19 17:06:44 +0700

    sd

--- api/submit.js
+++ api/submit.js
@@ -1,32 +1,34 @@
+
 import { MongoClient } from 'mongodb';
 
-// MongoDB connection URI
 const uri = 'mongodb+srv://duym5122015:&LFSauxL@cluster0.eld6y.mongodb.net/weeding?retryWrites=true&w=majority';
+let client;
+let clientPromise;
+
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  global._mongoClientPromise = client.connect();
+}
+clientPromise = global._mongoClientPromise;
 
 export default async (req, res) => {
   if (req.method === 'POST') {
-    const { name, message } = req.body;  // Extract the form data
-
-    // Create a new MongoClient
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const { name, message } = req.body;
 
     try {
-      await client.connect();  // Connect to MongoDB
+      const client = await clientPromise; // Reuse the connection
+      const database = client.db('damcuoi'); // Database name
+      const collection = database.collection('loichuc'); // Collection name
 
-      const database = client.db('weeding');  // Choose the 'wedding' database
-      const collection = database.collection('loichuc');  // Choose the 'loichuc' collection
-
-      // Insert the form data into the collection
-      const result = await collection.insertOne({ name, message, date: new Date() });
+      // Insert the form data
+      await collection.insertOne({ name, message, date: new Date() });
 
       res.status(200).send('Data has been saved successfully!');
     } catch (error) {
       console.error(error);
-      res.status(500).send(error);
-    } finally {
-      await client.close();  // Close the connection
+      res.status(500).send('Error saving data: ' + error.message);
     }
   } else {
-    res.status(405).send(error);
+    res.status(405).send('Method Not Allowed');
   }
-};
+}
